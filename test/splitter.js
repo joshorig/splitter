d51173086b88ca19a0668d8f0c65b71e31f6b05f
joshorig <cassidy.joshua@gmail.com>
2017-09-02 19:57:16 +0100

    Add odd wei test

--- test/splitter.js
+++ test/splitter.js
@@ -6,6 +6,7 @@ const Splitter = artifacts.require("./Splitter.sol");
 contract('Splitter', function(accounts) {
 
   const amountToSend = 50;
+  const oddAmountToSend = 5;
   const ownerAccount = accounts[0];
   const senderAccount = accounts[0];
   const enderAccount = accounts[4];
@@ -38,8 +39,28 @@ contract('Splitter', function(accounts) {
     updatedBalances[0] = await splitter.balances.call(recipientAccounts[0]);
     updatedBalances[1] = await splitter.balances.call(recipientAccounts[1]);
 
-    assert.equal(updatedBalances[0].valueOf(),recipientStartingBalances[0].add(amountToSend/2).valueOf(), "Did not split to first recipient");
-    assert.equal(updatedBalances[1].valueOf(),recipientStartingBalances[1].add(amountToSend/2).valueOf(), "Did not split to second recipient");
+    assert.equal(updatedBalances[0].valueOf(),recipientStartingBalances[0].add(~~(amountToSend/2)).valueOf(), "Did not split to first recipient");
+    assert.equal(updatedBalances[1].valueOf(),recipientStartingBalances[1].add(~~(amountToSend/2)).valueOf(), "Did not split to second recipient");
+
+  });
+
+  it("Should split odd wei remainder to sender", async () => {
+
+    let senderStartBalance = await splitter.balances.call(senderAccount);
+    let txObject = await splitter.split(recipientAccounts[0], recipientAccounts[1], { from: senderAccount, value: oddAmountToSend });
+
+    assert.equal(txObject.logs.length,1,"Did not log LogSplit event");
+    let logEvent = txObject.logs[0];
+    assert.equal(logEvent.event,"LogSplit","Did not LogSplit");
+    assert.equal(logEvent.args.sender,accounts[0],"Did not log sender correctly");
+    assert.equal(logEvent.args.firstRecipient,recipientAccounts[0],"Did not log firstRecipient correctly");
+    assert.equal(logEvent.args.secondRecipient,recipientAccounts[1],"Did not log secondRecipient correctly");
+    assert.equal(logEvent.args.amount.valueOf(),~~(oddAmountToSend/2),"Did not log amount correctly");
+
+
+    let senderUpdatedBalance = await splitter.balances.call(senderAccount);
+
+    assert.equal(senderUpdatedBalance.valueOf(),senderStartBalance.add(1).valueOf(), "Did not split odd wei remainder to sender");
 
   });
 
